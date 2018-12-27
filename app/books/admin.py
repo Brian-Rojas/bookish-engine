372925327d59777aec4689ec2655073b688c4083
Brian Rojas <rojasbrian.95@gmail.com>
2018-12-27 15:07:45 -0800

    initial migration and added admin page

--- app/books/admin.py
+++ app/books/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Note)
+admin.site.register(Quote)
+
+
 
 # Register your models here.
