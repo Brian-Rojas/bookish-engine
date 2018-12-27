2f32d2bbc066f454ebdf9269edf25827bc985b3b
Brian Rojas <rojasbrian.95@gmail.com>
2018-12-27 14:52:55 -0800

    finished adding model classes

--- app/books/models.py
+++ app/books/models.py
@@ -7,16 +7,16 @@ class Book(models.Model):
     notes = models.ForeignKey('Note', on_delete=models.CASCADE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     read = models.BooleanField(default=False)
-    date_purchased =
-    date_first_read =
-    date_last_read =
+    date_purchased = models.DateField(auto_now=False, auto_now_add=False)
+    date_first_read = models.DateField(auto_now=False, auto_now_add=False)
+    date_last_read = models.DateField(auto_now=False, auto_now_add=False)
+    date_added = models.DateTimeField(auto_now=False, auto_now_add=True)
     read_count = models.IntegerField(default=0)
     book_quotes = models.ForeignKey('Quote', on_delete=models.CASCADE)
     book_summary = models.TextField()
     favorite = models.BooleanField(default=False)
     isbn = models.CharField(max_length=20)
-    # barcode =
-    # similar_books
+    # similar_books =
     # image =
 
 
@@ -25,18 +25,18 @@ class Category(models.Model):
 
 
 class Note(models.Model):
-    # user =
-    date_created =
-    date_updated =
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    date_updated = models.DateField(auto_now=True, auto_now_add=True)
     text = models.TextField()
     title = models.CharField(max_length=200)
 
 
 class Quote(models.Model):
-    # user =
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    date_created =
-    date_updated =
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    date_updated = models.DateField(auto_now=True, auto_now_add=True)
     author = models.CharField(max_length=100)
     text = models.TextField()
     page_found = models.IntegerField(default=0)
