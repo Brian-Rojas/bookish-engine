ef32387f2f71b97fd6b7b9b66807548c2129e38c
Brian Rojas <rojasbrian.95@gmail.com>
2018-12-29 20:41:14 -0800

    added more viewsets and routes for diff models.

--- app/books/urls.py
+++ app/books/urls.py
@@ -4,6 +4,9 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('books', views.BookView)
+router.register('notes', views.NoteView)
+router.register('category', views.CategoryView)
+router.register('quotes', views.QuoteView)
 
 urlpatterns = [
     path('', include(router.urls))
